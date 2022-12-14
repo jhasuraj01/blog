e30f8800887f70d0a34a6e229e0ca3a993b763da
jhasuraj01 <44930179+jhasuraj01@users.noreply.github.com>
2022-12-14 06:00:57 +0000

    ignore .docusaurus

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/workspaces/blog/node_modules/infima/dist/css/default/default.css'),
-  require('/workspaces/blog/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/workspaces/blog/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/workspaces/blog/src/css/custom.css'),
-];
