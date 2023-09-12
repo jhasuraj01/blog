cab3421fa40dbd5b9840d982357a776d04da9af9
jhasuraj01 <44930179+jhasuraj01@users.noreply.github.com>
2023-09-12 11:13:35 +0530

    feat: moved clarity tracking code to config file

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -28,6 +28,19 @@ const config = {
     locales: ['en'],
   },
 
+  headTags: [
+    {
+      tagName: 'script',
+      attributes: {
+        type: 'text/javascript',
+      },
+      innerHTML:  '(function(c,l,a,r,i,t,y){\n' +
+                  '    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};\n' +
+                  '    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;\n' +
+                  '    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);\n' +
+                  '})(window, document, "clarity", "script", "h8s7eoq3zy");',
+    },
+  ],
   markdown: {
     mermaid: true,
   },
